8ebfef4ce0028235a4faba83ca7dcbcd619019e4
fawazming <fawazming@gmail.com>
2024-06-16 10:59:56 +0100

    INIT

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/gallery', 'Home::gallery');
+$routes->get('/blog', 'Home::blog');
+$routes->get('/blog/(:any)/(:any)/(:any)', 'Home::singleBlog/$1/$2/$3');
+$routes->get('/blog/(:any)', 'Home::blogD/$1');
+$routes->get('/tests', 'Home::tests');
+$routes->get('/(:any)', 'Home::pages/$1');
