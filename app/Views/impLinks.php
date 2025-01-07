1eb7f56f8def30ce114a513be722b182a748cf91
Fawaz <fawazming@gmail.com>
2025-01-07 12:37:13 +0100

    `Added links to the impLinks.php view`

--- app/Views/impLinks.php
+++ app/Views/impLinks.php
@@ -6,16 +6,24 @@
         </div>
     </header>
     <section class="sabbi-section stage-future_member">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-9 px-10">
-                    <?=$pg[1]['setting_value']?>
-                </div>
-                <aside class="col-sm-3">
-                    <a href="" class="btn btn-gules docs-creator btn-block">Access Journal</a>
-                    <a href="" class="btn btn-gules docs-creator btn-block">Access Journal</a>
-                </aside>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 px-10">
+                <?=$pg[1]['setting_value']?>
             </div>
+            <aside class="col-sm-3">
+                <a href="<?=base_url('plagiarism-policies')?>" class="btn btn-gules docs-creator btn-block">Plagiarism Policies</a>
+                <a href="<?=base_url('publication-ethics')?>" class="btn btn-gules docs-creator btn-block">Publication Ethics</a>
+                <a href="<?=base_url('editors-responsibilities')?>" class="btn btn-gules docs-creator btn-block">Editors' Responsibilities</a>
+                <a href="<?=base_url('authors-duties')?>" class="btn btn-gules docs-creator btn-block">Authors' Duties</a>
+                <a href="<?=base_url('reviewers-duties')?>" class="btn btn-gules docs-creator btn-block">Reviewers' Duties</a>
+                <a href="<?=base_url('instructions-for-authors')?>" class="btn btn-gules docs-creator btn-block">Instructions for Authors</a>
+                <a href="<?=base_url('article-policies')?>" class="btn btn-gules docs-creator btn-block">Article Policies</a>
+                <a href="<?=base_url('editing-services')?>" class="btn btn-gules docs-creator btn-block">Editing Services</a>
+                <a href="<?=base_url('peer-review-policy')?>" class="btn btn-gules docs-creator btn-block">Peer Review Policy</a>
+                <a href="<?=base_url('default')?>" class="btn btn-gules docs-creator btn-block">Default (404 Not Found)</a>
+            </aside>
         </div>
+    </div>
     </section><!-- /.stage-future_member -->
     
