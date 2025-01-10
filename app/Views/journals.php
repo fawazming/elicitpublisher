336ca88c35d04940d386b55840d8576481d4ab54
Fawaz <fawazming@gmail.com>
2025-01-10 13:41:28 +0100

    `Refactor journal display layout: changed column width from 4 to 3 columns on medium devices.`

--- app/Views/journals.php
+++ app/Views/journals.php
@@ -1,7 +1,7 @@
 <h2 class="title text-center">Our Journals</h2>
             <div class="row home-info-block-first" style="display: flex; justify-content: center; flex-wrap: wrap;">
                 <?php foreach ($journals as $journal):?>
-                <div class="col-md-4 col-sm-6">
+                <div class="col-md-3 col-sm-6">
                     <a href="<?=base_url()?>journal/<?=$journal->id?>">
                         <article class="sabbi-thumlinepost-card solitude-bg__x">
                             <figure class="sabbi-thumlinepost-card-figure">
